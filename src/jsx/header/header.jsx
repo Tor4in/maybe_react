45fc383897a5631aaaedeaf5cae719702c487d5b
Tor4in <tor4in@MacBook-Air-Tor4in.local>
2023-03-11 21:34:43 +0200

    some fixes or add

--- src/jsx/header/header.jsx
+++ src/jsx/header/header.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import s from './header.module.css'
 import i from "./media/logo_img.jpg"
+import '../../zerostyles.css'
 import { Link } from "react-router-dom";
 
 
 const Header = () => {
+    let anim_el = React.createRef()
+    let bg_manu = React.createRef()
+
+    let animAdd = () => {
+        if (anim_el) {
+
+            anim_el.current.classList.toggle('anim_active')
+            bg_manu.current.classList.toggle('brg_bg')
+            bg_manu.current.classList.toggle('add_off')
+        }
+
+    }
+
+
+
     return (
         <header >
 
@@ -16,7 +32,7 @@ const Header = () => {
 
             </div>
             <div className={s.div_nav__list}>
-                <ul >
+                <ul className={s.main_ul}>
                     <li> <Link to='/'>Головна</Link>  </li>
                     <li> <Link to='/sachen'>Товари</Link>
                         <div className={s.bar}>
@@ -36,6 +52,35 @@ const Header = () => {
                     <li> <Link to='/referal_form' >Партнерська програма</Link> </li>
                     <li> <Link to='/faq' >FAQ</Link> </li>
                 </ul>
+                <div ref={anim_el} className={s.brg_logo} onClick={animAdd}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+
+                </div>
+                <div className="add_off" ref={bg_manu}>
+
+                    <ul className={s.brg_ul_list}>
+                        <li> <Link to='/'>Головна</Link>  </li>
+                        <li> <Link to='/sachen'>Товари</Link>
+                            <div className={s.bar}>
+                                <ul>
+                                    <li>Airpods 2</li>
+                                    <li>Airpods 3 </li>
+                                    <li>Airpods pro </li>
+                                    <li>Watch GS8 mini</li>
+                                    <li>Watch GS8+ Ultra</li>
+                                    <li>XO PR1 30</li>
+                                    <li>Powerbank</li>
+                                </ul>
+                            </div>
+
+                        </li>
+                        <li> <Link to='/info' >Інфо</Link> </li>
+                        <li> <Link to='/referal_form' >Партнерська програма</Link> </li>
+                        <li> <Link to='/faq' >FAQ</Link> </li>
+                    </ul>
+                </div>
             </div>
         </header>
 
--- src/jsx/main/main.jsx
+++ src/jsx/main/main.jsx
@@ -9,7 +9,7 @@ function Main() {
 
 
   let el1 = React.createRef()
-  // let el2 = React.createRef()
+  
   let imgel = React.createRef()
   let sachel = React.createRef()
 
@@ -17,7 +17,7 @@ function Main() {
 
     if (el1 && imgel) {
       el1.current.classList.toggle('add_off')
-      // el2.current.classList.toggle('add_off')
+      
       imgel.current.classList.toggle('rotate')
       sachel.current.classList.toggle('add_off')
       sachel.current.classList.toggle('add')
@@ -34,7 +34,7 @@ function Main() {
 
     <div className={s.main_wraper}>
       <div className={s.wraper_title}>
-        
+
         <p className={s.main_name}> maybe</p>
         <p className={s.main_name_sub}>Обери товар, створи магазин, починай продавати.</p>
       </div>
@@ -65,20 +65,17 @@ function Main() {
             <p>700</p>
           </Link>
         </div>
-        {/* <div ref={el2} className='add_off'>
-          <Link to="/sachen">
-            <div></div>
-            <p>250</p>
-          </Link>
-        </div> */}
+        
+      </div>
+      <div className={s.add_wrap}>
+        <div className={s.add} ref={imgel} >
+          <img src={svg} alt="" onClick={show} />
+        </div>
+        <div className="add_off" ref={sachel}>
+          <Link to='/sachen'> <img src={svg} alt="" onClick={show} /></Link>
+        </div>
       </div>
 
-      <div className={s.add} ref={imgel} >
-        <img src={svg} alt="" onClick={show} />
-      </div>
-      <div className="add_off" ref={sachel}>
-        <Link to='/sachen'> <img src={svg} alt="" onClick={show} /></Link>
-      </div>
       <div className={s.info_bank}>
         <p>Виникли питання?</p>
 
